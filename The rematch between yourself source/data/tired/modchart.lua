bb3624b523e07554dd28c4b10eda5d2ac3349e65
DavidPootis <83901746+DavidPootis@users.noreply.github.com>
2021-06-05 06:54:55 +0200

    Wtf are these images lmao.
    Probably me testing the makeSprite function lmaooo

---
+++ The rematch between yourself source/data/tired/modchart.lua
@@ -0,0 +1,64 @@
+function start (song) --Dude sometimes I forget to put the end in the last code line and the game crashes and I have to open it again aaaaaa
+setCamZoom(0.9) --Camera a litte bit far to see the zero zone corrupt the enviroment
+setHudZoom(0.9) --Have you seen the kapi mod? It's pretty cool
+end --Yo shut up im listening to a air conditioner.
+function update (elapsed) --WHY IS KAPI SO CUTE LMAOO
+if curStep > 1 and curStep < 5 then --im not a furry ok
+tweenAngle(0, -45, 1, done) --TAGM BRO YOU ARE SO COOL DUDE
+tweenAngle(3, 45, 1, done) --bone work
+tweenPos(1, 275, 50, 1, done) --DO A FLIP
+tweenPos(2, 165, 50, 1, done) --Is this confusing?
+tweenAngle(4, -45, 1, done) --Just a little bit
+tweenAngle(7, 45, 1, done) --Yo kade has support for 1000+ bpm wtf??
+tweenPos(5, 932, 50, 1, done) --idk anymore i will stop putting these dumb comments
+tweenPos(6, 815, 50, 1, done) --among
+end
+if curStep > 5 and curStep < 143 then
+tweenPos(0, 75, 165, 1, done)
+tweenPos(3, 350, 165, 1, done)
+tweenPos(4, 735, 165, 1, done)
+tweenPos(7, 1000, 165, 1, done)
+end
+if curStep > 143 and curStep < 368 then
+tweenPos(0, 100, 50, 1, done)
+tweenPos(3, 350, 50, 1, done)
+tweenPos(1, 275, 165, 1, done)
+tweenPos(2, 165, 165, 1, done)
+tweenPos(4, 750, 50, 1, done)
+tweenPos(5, 932, 165, 1, done)
+tweenPos(6, 815, 165, 1, done)
+tweenPos(7, 1000, 50, 1, done)
+end
+if curStep > 368 and curStep < 512 then --sike
+tweenPos(0, 165, 165, 1, done)
+tweenPos(2, 100, 50, 1, done)
+tweenPos(4, 815, 165, 1, done)
+tweenPos(6, 750, 50, 1, done)
+end
+if curStep > 512 and curStep < 655  then
+tweenPos(1, 350, 50, 1, done)
+tweenPos(3, 275, 165, 1, done)
+tweenPos(5, 1000, 50, 1, done)
+tweenPos(7, 932, 165, 1, done)
+end
+if curStep > 655 and curStep < 767 then
+tweenAngle(1, 90, 1, done)
+tweenAngle(2, 450, 1, done)
+tweenAngle(5, 90, 1, done)
+tweenAngle(6, 450, 1, done)
+end
+if curStep > 767 and curStep < 1128 then
+tweenAngle(0, 0, 1, done)
+tweenAngle(1, 0, 1, done)
+tweenAngle(2, 0, 1, done)
+tweenAngle(3, 0, 1, done)
+tweenAngle(4, 0, 1, done)
+tweenAngle(5, 0, 1, done)
+tweenAngle(6, 0, 1, done)
+tweenAngle(7, 0, 1, done)
+end
+if curStep > 1128 and curStep < 1184 then
+setHudPosition(2000, 4000)
+setCamPosition(-2000, -4000)
+	end --Should i take a break?
+end --Nah im good
