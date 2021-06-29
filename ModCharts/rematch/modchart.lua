86a742cde5fd9c08b61b93985b12f7535c0e19df
DavidPootis <83901746+DavidPootis@users.noreply.github.com>
2021-06-29 04:50:54 +0200

    Funny modcharts.

---
+++ ModCharts/rematch/modchart.lua
@@ -0,0 +1,24 @@
+function update (elapsed)
+	if curStep >= 0 and curStep < 480 then --TAGM you got some mods showcase to do buddy.Hey but seriously you are so cool dude :)
+    setActorY(300, 6)
+	setActorY(150, 5)
+	setActorY(50, 7)
+	setActorY(300, 2)
+	setActorY(150, 1)
+	setActorY(50, 3)
+	end
+	if curStep >= 481 and curStep < 1000 then
+	tweenPosYAngle(0, 0, 0, 3, done) --WHY TF AM I PUTTING THESE COMMENTS??
+	tweenPosYAngle(1, 300, 0, 3, done) --I WANT TO SHIT MYSELF
+	tweenPosYAngle(2, 150, 0, 3, done) --LUA IS COOL I GUESS
+	tweenPosYAngle(3, 10, 0, 3, done) --If you are actually reading this.Thanks you I lov u :)
+	tweenPosYAngle(4, 0, 0, 3, done) --Did you know that bfake's name is Jack?
+	tweenPosYAngle(5, 300, 0, 3, done) --Sandwich
+	tweenPosYAngle(6, 150, 0, 3, done) --MY GOD THIS SANDWICH IS SO GOOD
+	tweenPosYAngle(7, 10, 0, 3, done) --Ayo wtf
+	end
+end
+
+
+
+
